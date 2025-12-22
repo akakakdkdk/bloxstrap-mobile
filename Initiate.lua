87561f8cda472deed4059d715c459e165e488bda
akakakdkdk <cardosodavi0312@gmail.com>
2025-12-22 15:56:19 -0300

    Add FPS boost settings for mobile in Initiate.lua
    This script initiates custom settings for FPS boost on mobile devices by adjusting rendering quality and lighting properties.

---
+++ Initiate.lua
@@ -0,0 +1,25 @@
+-- INITIATE CUSTOM - FPS BOOST MOBILE
+
+pcall(function()
+	setfpscap(120)
+end)
+
+pcall(function()
+	settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+end)
+
+local Lighting = game:GetService("Lighting")
+
+Lighting.Technology = Enum.Technology.Compatibility
+Lighting.GlobalShadows = false
+Lighting.Brightness = 1
+Lighting.ExposureCompensation = -0.5
+Lighting.EnvironmentDiffuseScale = 0
+Lighting.EnvironmentSpecularScale = 0
+Lighting.ShadowSoftness = 0
+
+for _, v in pairs(Lighting:GetChildren()) do
+	if v:IsA("PostEffect") or v:IsA("Atmosphere") then
+		v:Destroy()
+	end
+end
